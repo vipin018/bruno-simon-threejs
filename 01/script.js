cf29661e378f8121da60f2a07dcfeee398a5c752
vipin018 <vipinpathak018@gmail.com>
2025-02-09 10:58:29 +0530

    updated

--- 01/script.js
+++ 01/script.js
@@ -1,1 +1,23 @@
-console.log("Hello World");
+import * as THREE from 'three';
+
+// scene
+const scene = new THREE.Scene();
+// it is like a container that holds all the objects.
+
+// geometry
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+// it is the shape of the object which in this case is a cube.
+
+// material
+const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+// it is the material of the object which in this case is a blue color.
+// meshmaterial is the combination of geometry and material
+
+// mesh
+const mesh = new THREE.Mesh(geometry, material);
+// it is the combination of geometry, material and position.
+
+// add the mesh to the scene
+scene.add(mesh);
+
+
