50413d099fe93444810d1e66a3d44cfe18e32d08
vipin018 <vipinpathak018@gmail.com>
2025-02-24 21:48:57 +0530

    updated

--- transformation & animation/main.js
+++ transformation & animation/main.js
@@ -1,17 +1,34 @@
 import * as THREE from 'three';
 
+
+// canvas
+const canvas = document.querySelector('#canvas');
+
+// scene
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-const geometry = new THREE.BoxGeometry(2, 2, 2);
-const material = new THREE.MeshBasicMaterial({ color: 0x8aff99 });
+// object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshNormalMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 5;
+// sizes
+const sizes = {
+  width: 800,
+  height: 600
+}
 
+// camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+camera.position.z = 3;
+scene.add(camera);
+
+// renderer
+const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+renderer.setSize(sizes.width, sizes.height);
+
+// render
 renderer.render(scene, camera);
+
+
