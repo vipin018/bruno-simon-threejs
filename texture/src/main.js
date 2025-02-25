f86dfc12b9488a16eb0f71518deff3ba48d7dc4a
vipin018 <vipinpathak018@gmail.com>
2025-02-25 13:29:47 +0530

    updated

--- texture/src/main.js
+++ texture/src/main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 // import { GUI } from 'lil-gui';
+
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 // Scene
 const scene = new THREE.Scene();
@@ -14,12 +15,43 @@ const loader = new THREE.TextureLoader();
 // const roughnessTexture = loader.load('./textures/door/roughness.jpg');
 // const minecraftTexture = loader.load('./textures/minecraft.png');
 // const checkerboardTexture = loader.load('./textures/checkerboard-1024x1024.png');
-const checkerboardTexture = loader.load('./textures/checkerboard-8x8.png');
+// const checkerboardTexture = loader.load('./textures/checkerboard-8x8.png');
 
+// lights
+const ambientLight = new THREE.AmbientLight(0xffffff, 15);
+// scene.add(ambientLight);
+
+const directionalLight1 = new THREE.DirectionalLight(0xafafaf, 20);
+directionalLight1.position.set(3,3,3);
+scene.add(directionalLight1);
+const directionalLight2 = new THREE.DirectionalLight(0xafafaf, 20);
+directionalLight2.position.set(-3,3,3);
+scene.add(directionalLight2);
+
+const directionalLightHelper1 = new THREE.DirectionalLightHelper(directionalLight1, 1);
+scene.add(directionalLightHelper1);
+const directionalLightHelper2 = new THREE.DirectionalLightHelper(directionalLight2, 5);
+scene.add(directionalLightHelper2);
+
+const pointLight = new THREE.PointLight(0xafafaf, 200, 50, 1);
+pointLight.position.set(0,0,5);
+pointLight.castShadow = true;
+
+scene.add(pointLight);
+const pointHelper = new THREE.PointLightHelper(pointLight, 1);
+scene.add(pointHelper);
 
 // Object
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ map: checkerboardTexture });
+const material = new THREE.MeshStandardMaterial({
+   color: '#a96666',
+  metalness: 1,
+  roughness: 0.2,
+  emissive: 'white',
+  emissiveIntensity: 0.1,
+  vertexColors: true,
+ 
+ });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
