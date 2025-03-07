5f0b0fc591abc64862c0167403d5e2a450388a95
vipin018 <vipinpathak018@gmail.com>
2025-03-07 23:46:21 +0530

    updated

---
+++ scroll_animation/src/main.js
@@ -0,0 +1,58 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+// canvas
+const canvas = document.querySelector('.webgl');
+
+
+// scene
+const scene = new THREE.Scene();
+// it is like a container that holds all the objects.
+
+// geometry
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+// it is the shape of the object which in this case is a cube.
+
+// material
+const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+// it is the material of the object which in this case is a blue color.
+// meshmaterial is the combination of geometry and material
+
+// mesh
+const mesh = new THREE.Mesh(geometry, material);
+
+
+// add the mesh to the scene
+scene.add(mesh);
+
+// sizes
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+// camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+// renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+})
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
+
+// controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+// animation
+const clock = new THREE.Clock();
+
+function animate() {
+    controls.update();
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+    const elapsedTime = clock.getElapsedTime();
+    mesh.rotation.y = elapsedTime;
+}
+animate();
