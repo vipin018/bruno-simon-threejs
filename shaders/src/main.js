410bc792d1165f90bf0a1bae6eaddd41142bb6c9
vipin018 <vipinpathak018@gmail.com>
2025-03-11 12:15:22 +0530

    updated

--- shaders/src/main.js
+++ shaders/src/main.js
@@ -1,12 +1,16 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GUI } from 'lil-gui';
 import vertexShader from './shaders/test/vertex.glsl';
 import fragmentShader from './shaders/test/fragment.glsl';
 
+const gui = new GUI({
+  width: 100,
+});
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
-
+camera.position.set(0, 0, 5);
+// camera.lookAt(0, 0, 0);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
@@ -17,10 +21,13 @@ const ambientLight = new THREE.AmbientLight(0xffffff, 1.5);
 scene.add(ambientLight);
 
 // Cube
-const geometry = new THREE.PlaneGeometry(5, 5, 32, 32);
+const geometry = new THREE.PlaneGeometry(2, 2, 32, 32);
 const material = new THREE.RawShaderMaterial({
   vertexShader,
   fragmentShader,
+  // wireframe: true,
+  side: THREE.DoubleSide,
+  transparent: true,
 });
 const plane = new THREE.Mesh(geometry, material);
 scene.add(plane);
--- shaders/src/shaders/test/vertex.glsl
+++ shaders/src/shaders/test/vertex.glsl
@@ -1,9 +1,19 @@
-uniform mat4 projectionMatrix;
-uniform mat4 viewMatrix;
-uniform mat4 modelMatrix;
+uniform mat4 projectionMatrix; //this is the projection matrix which is used to project the 3D world onto the 2D screen
+uniform mat4 viewMatrix; //this is the view matrix which is used to view the 3D world from a specific point
+uniform mat4 modelMatrix; //this is the model matrix which is used to position the 3D object in the 3D world
 
-attribute vec3 position;
+// mat1, mat2, mat3, mat4 are the types of matrices in glsl
+
+attribute vec3 position; //this is the position of the vertex in the 3D world
 
 void main() {
-    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    modelPosition.z += sin(modelPosition.x * 10.0) * 0.1;
+
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectionPosition = projectionMatrix * viewPosition;
+
+    gl_Position = projectionPosition; // this is the final position of the vertex in the 3D world
+
 }
