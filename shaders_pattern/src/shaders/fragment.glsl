d2d3deeb69071fc0fde9eecbde89c5d3c5dfc7b5
vipin018 <vipinpathak018@gmail.com>
2025-03-11 21:31:42 +0530

    updated

--- shaders_pattern/src/shaders/fragment.glsl
+++ shaders_pattern/src/shaders/fragment.glsl
@@ -1,4 +1,14 @@
-void main()
-{
-    gl_FragColor = vec4(0.5, 0.0, 1.0, 1.0);
+varying vec2 vUv;
+
+void main() {
+
+    float strength = mod(vUv.y*10.0, 1.0 );
+
+if(strength < 0.5) {
+    strength = 0.0 ;
+}else{
+    strength = strength;
+}
+
+    gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
