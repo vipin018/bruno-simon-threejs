28ff25099db806a4da7a325ba6db048f3f5b1bb5
vipin018 <vipinpathak018@gmail.com>
2025-03-21 14:52:49 +0530

    updated

---
+++ galaxy-shader/src/shaders/vertex.glsl
@@ -0,0 +1,14 @@
+uniform float uSize;
+
+attribute float aScale;
+
+void main() {
+
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectionPosition = projectionMatrix * viewPosition;
+    gl_Position = projectionPosition;
+
+    gl_PointSize = uSize * aScale;
+  
+}
