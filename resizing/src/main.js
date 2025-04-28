0fa59281e24316b739ba3a1cd6fa52a994592fe8
vipin018 <vipinpathak018@gmail.com>
2025-04-28 14:14:51 +0530

    added double click full screen

--- resizing/src/main.js
+++ resizing/src/main.js
@@ -3,19 +3,44 @@ import * as THREE from 'three';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-
 const canvas = document.querySelector('#canvas');
 const renderer = new THREE.WebGLRenderer({canvas});
 
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
+const size = {
+  width: window.innerWidth,
+  height: window.innerHeight
+};
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({color: "#FBDDDB"});
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+scene.background = new THREE.Color("ghostwhite");
+cube.scale.set(1.5, 1.5, 1.5);
 
 camera.position.z = 5;
+
+window.addEventListener('resize', () => {
+  size.width = window.innerWidth;
+  size.height = window.innerHeight;
+  
+  camera.aspect = size.width / size.height;
+  camera.updateProjectionMatrix();
+  
+  renderer.setSize(size.width, size.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+window.addEventListener("dblclick", () => {
+  if (!document.fullscreenElement) {
+    canvas.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
+});
 
 function animate() {
     requestAnimationFrame(animate);
