af6eca7077b58759ff636dc20d5896e6093f2554
vipin018 <vipinpathak018@gmail.com>
2025-04-29 12:51:47 +0530

    adding lights to the scene

--- lights/src/main.js
+++ lights/src/main.js
@@ -1,98 +1,77 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GUI } from 'lil-gui';
-import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper';
+
 const canvas = document.querySelector('.webgl');
 
 const scene = new THREE.Scene();
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 1.5);
+/**
+ * Lights
+ */
+
+// AMBIENT LIGHT
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
 scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.5);
-directionalLight.position.set(1, 1, 0);
+// DIRECTIONAL LIGHT
+const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.3);
 scene.add(directionalLight);
 
-const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 6);
-hemisphereLight.position.set(0, 2, 0);
+// HEMISPHERICAL LIGHT
+const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.9);
 scene.add(hemisphereLight);
 
-const pointLight = new THREE.PointLight(0xff9000, 5, 10, 1);
-pointLight.position.set(1, 0.5, 1);
+// POINT LIGHT
+const pointLight = new THREE.PointLight(0xff9000, 0.5);
 scene.add(pointLight);
 
-const rectAreaLight = new THREE.RectAreaLight(0x4e00ff, 1, 1, 1);
+// RECTANGLE LIGHT
+const rectAreaLight = new THREE.RectAreaLight(0x4e00ff, 2, 0.5, 1);
+scene.add(rectAreaLight);
 rectAreaLight.position.set(-1.5, 0, 1.5);
 rectAreaLight.lookAt(new THREE.Vector3());
-scene.add(rectAreaLight);
 
-const spotLight = new THREE.SpotLight(0x78ff00, 10, 10, Math.PI * 0.1, 0.5, 1);
+// SPOT LIGHT
+const spotLight = new THREE.SpotLight(0x78ff00, 1, 10, Math.PI *0.1, 0.25, 1);
 spotLight.position.set(0, 2, 3);
-scene.add(spotLight)
+scene.add(spotLight);
 
-const hemisphereLightHelper = new THREE.HemisphereLightHelper(hemisphereLight,0.5);
-scene.add(hemisphereLightHelper);
 
-const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight,0.5);
-scene.add(directionalLightHelper);
 
-const pointLightHelper = new THREE.PointLightHelper(pointLight,0.5);
-scene.add(pointLightHelper);
 
-const spotLightHelper = new THREE.SpotLightHelper(spotLight);
-scene.add(spotLightHelper);
-
-const rectAreaLightHelper = new RectAreaLightHelper(rectAreaLight);
-scene.add(rectAreaLightHelper);
-
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+const geometry = new THREE.BoxGeometry(0.75, 0.75, 0.75);
 const material = new THREE.MeshStandardMaterial({
-    color: 0xffffff,
-    roughness: 0,
-    // metalness: 1,
-
+    roughness: 0.4,
 });
+
 
 const cube = new THREE.Mesh(geometry, material);
 cube.position.y = 0.5; // Positioning above the plane
-scene.add(cube);
+scene.add(cube)
 
-// Sphere
 const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(0.6, 16, 16),
-    new THREE.MeshStandardMaterial({
-        color: 0xffffff,
-        roughness: 0,
-        // metalness: 1,
-    })
+    new THREE.SphereGeometry(0.5, 32, 32),
+    material
 );
-sphere.position.set(-1.5, 0.5, 0);
+sphere.position.x = -1.5;
+sphere.position.y = 0.5;
 scene.add(sphere);
 
-// Torus
 const torus = new THREE.Mesh(
-    new THREE.TorusGeometry(0.5, 0.2, 16, 60),
-    new THREE.MeshStandardMaterial({
-        color: 0xffffff,
-        roughness: 0,
-        // metalness: 1,
-
-    })
+    new THREE.TorusGeometry(0.3, 0.2, 16, 60),
+    material
 );
-torus.position.set(2, 0.5, 0);
+torus.position.x = 2;
+torus.position.y = 0.5;
 scene.add(torus);
 
-// Plane (Ground)
 const plane = new THREE.Mesh(
     new THREE.PlaneGeometry(8, 8),
-    new THREE.MeshStandardMaterial({
-        color: 0x808080,
-        roughness: 0.3,
-
-    })
+    material
 );
 plane.rotation.x = -Math.PI / 2; // Make it horizontal
-plane.position.y = -0.2; // Position it below the objects
+plane.position.y = 0; // Position it below the objects
 scene.add(plane);
 
 // Camera
@@ -101,9 +80,16 @@ const sizes = {
     height: window.innerHeight
 };
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 5;
-camera.position.y = 2;
+camera.position.z = 2;
+camera.position.y = 1;
+camera.position.x = 2;
 scene.add(camera);
+
+if (window.innerWidth < 768) {
+    camera.position.z = 4;
+    camera.position.y = 1;
+    camera.position.x = 4;
+}
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({ canvas: canvas });
@@ -123,6 +109,7 @@ function animate() {
     cube.rotation.x = clock.getElapsedTime() / 2;
     sphere.rotation.x = clock.getElapsedTime() / 2;
     torus.rotation.x = clock.getElapsedTime() / 2;
+
 }
 animate();
 
@@ -135,8 +122,44 @@ window.addEventListener('resize', () => {
     renderer.setSize(sizes.width, sizes.height);
 });
 
+// GUI
 const gui = new GUI();
-gui.add(ambientLight, 'intensity').min(0).max(10).step(0.01);
-gui.add(directionalLight, 'intensity').min(0).max(10).step(0.01);
-gui.add(hemisphereLight, 'intensity').min(0).max(10).step(0.01);
-gui.add(pointLight, 'intensity').min(0).max(10).step(0.01);
+const cameraFolder = gui.addFolder('Camera');
+cameraFolder.add(camera.position, 'x').min(0).max(10).step(0.01);
+cameraFolder.add(camera.position, 'y').min(0).max(10).step(0.01);
+cameraFolder.add(camera.position, 'z').min(0).max(10).step(0.01);
+cameraFolder.close();
+
+const ambientLightFolder = gui.addFolder('Ambient Light');
+ambientLightFolder.add(ambientLight, 'intensity').min(0).max(5).step(0.01);
+ambientLightFolder.add(ambientLight, 'visible');
+ambientLightFolder.close();
+
+const directionalLightFolder = gui.addFolder('Directional Light');
+directionalLightFolder.add(directionalLight, 'intensity').min(0).max(5).step(0.01);
+directionalLightFolder.add(directionalLight.position, 'x').min(-10).max(10).step(0.01);
+directionalLightFolder.add(directionalLight.position, 'y').min(-10).max(10).step(0.01);
+directionalLightFolder.add(directionalLight.position, 'z').min(-10).max(10).step(0.01);
+directionalLightFolder.add(directionalLight, 'visible');
+directionalLightFolder.close();
+
+const hemisphereLightFolder = gui.addFolder('Hemisphere Light');
+hemisphereLightFolder.add(hemisphereLight, 'intensity').min(0).max(5).step(0.01);
+hemisphereLightFolder.add(hemisphereLight, 'visible');
+hemisphereLightFolder.close();
+
+const pointLightFolder = gui.addFolder('Point Light');
+pointLightFolder.add(pointLight, 'intensity').min(0).max(5).step(0.01);
+pointLightFolder.add(pointLight.position, 'x').min(-10).max(10).step(0.01);
+pointLightFolder.add(pointLight.position, 'y').min(-10).max(10).step(0.01);
+pointLightFolder.add(pointLight.position, 'z').min(-10).max(10).step(0.01);
+pointLightFolder.add(pointLight, 'visible');
+pointLightFolder.close();
+
+const rectAreaLightFolder = gui.addFolder('Rect Area Light');
+rectAreaLightFolder.add(rectAreaLight, 'intensity').min(0).max(5).step(0.01);
+rectAreaLightFolder.add(rectAreaLight.position, 'x').min(-10).max(10).step(0.01);
+rectAreaLightFolder.add(rectAreaLight.position, 'y').min(-10).max(10).step(0.01);
+rectAreaLightFolder.add(rectAreaLight.position, 'z').min(-10).max(10).step(0.01);
+rectAreaLightFolder.add(rectAreaLight, 'visible');
+rectAreaLightFolder.close();
